118e57bae27ea91294f6a026459ace1ffdc0289a
camila gallegos <camigm1@gmail.com>
2020-09-11 22:18:41 -0600

    Solved ChallengeSets

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -1,36 +1,43 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
 namespace ChallengeSets
 {
     public class ChallengeSet01
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            return num1 == num2;
         }
+            
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1<number2)
+            {
+                return number1;
+            }
+                return number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1*factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return (nameOfPerson == "") ? "Hello!" : $"Hello, {nameOfPerson}!";
         }
 
         public string GetHey()
